d77361d5d6581423f9c6b5329bbb1f43e03cf217
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-07-01 15:45:34 +0200

    Add simple optimizer with no sanity checks
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- sigop-core/src/lib.rs
+++ sigop-core/src/lib.rs
@@ -1,8 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod optimizer {
+    use ethabi::ParamType;
+    use std::fs;
+
+    pub fn run(function_name: &str, params: &[ParamType]) -> Option<String> {
+        let suffixes = fs::read_to_string("db.txt").expect("Something went wrong reading the file");
+
+        for suffix in suffixes.lines() {
+            let new_function_name = format!("{}_{}", function_name, suffix);
+            let encoded_new_function_signature =
+                ethabi::short_signature(new_function_name.as_str(), params);
+
+            if encoded_new_function_signature[0] == 0 && encoded_new_function_signature[1] == 0 {
+                return Some(new_function_name);
+            }
+        }
+        None
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn it_optimize_function_signature() {
+            let function_name = "myFunction";
+            let function_params = [ParamType::Address];
+
+            let res = run(function_name, &function_params);
+            assert_eq!(true, res.is_some())
+        }
     }
 }
