77c1b862b6900b31de2970635af85f67eb361ac0
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-07-02 20:05:47 +0200

    Update code to optimize performance
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- sigop-cli/src/main.rs
+++ sigop-cli/src/main.rs
@@ -5,12 +5,15 @@ use sigop_core::optimizer::run;
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
+    /// The function signature to optimize.
     #[clap(short, long)]
     signature: String,
 
+    /// The maximum size of the suffix following the original function name.
     #[clap(short, long, default_value_t = 3)]
     level: u8,
 
+    /// The number of zero-bytes you want to have at the beginning of the optimized function.
     #[clap(short, long, default_value_t = 2)]
     target: u8,
 }
