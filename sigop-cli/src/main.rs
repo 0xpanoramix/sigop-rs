13e62af800c20c1cefb903991bdecd4982b32faf
Luca G.F <luca.georges-francois@epitech.eu>
2022-07-02 15:32:44 +0200

    Add simple optimizer (#2)

--- sigop-cli/src/main.rs
+++ sigop-cli/src/main.rs
@@ -1,3 +1,14 @@
+use sigop_core::optimizer::run;
+
 fn main() {
-    println!("Hello, world!");
+    let optimized = run("myFunction(address)", 3, 2);
+
+    match optimized {
+        None => {
+            println!("Either none optimization was found or an error has occurred")
+        }
+        Some(res) => {
+            println!("Found this optimization: {}", res)
+        }
+    }
 }
