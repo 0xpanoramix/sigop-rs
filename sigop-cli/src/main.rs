832e36bb3e642365151a01e6b0bbe7b179a2a6dd
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-07-02 15:43:29 +0200

    Add clap CLI configuration
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- sigop-cli/src/main.rs
+++ sigop-cli/src/main.rs
@@ -1,7 +1,22 @@
+use clap::Parser;
 use sigop_core::optimizer::run;
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(short, long)]
+    signature: String,
+
+    #[clap(short, long, default_value_t = 3)]
+    level: u8,
+
+    #[clap(short, long, default_value_t = 2)]
+    target: u8,
+}
+
 fn main() {
-    let optimized = run("myFunction(address)", 3, 2);
+    let cli = Cli::parse();
+    let optimized = run(&cli.signature, cli.level, cli.target);
 
     match optimized {
         None => {
